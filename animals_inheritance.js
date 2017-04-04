55fb2c0ccf58e4e99e39eb6ba80e7aded2c0690b
oscarhermawan <franky1790@gmail.com>
2017-04-04 11:38:27 +0700

    classical-inheritance-done

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,67 @@
 "use strict"
+
+
+class Animal{
+  constructor(legs, is_warm_blooded){
+    this._numlegs = legs;
+    this._is_warm_blooded = is_warm_blooded;
+  }
+
+  setNumLegs(legs){
+    return this._numlegs = legs;
+  }
+}
+
+class Frog extends Animal{
+  constructor(legs, is_warm_blooded){
+      super(legs, is_warm_blooded)
+      this._superPower = new SuperPower();
+  }
+}
+
+class Bat extends Animal{
+  constructor(legs, is_warm_blooded){
+      super(legs, is_warm_blooded)
+      this._superPower = new SuperPower();
+  }
+
+  get parent(){
+    return this._parent
+  }
+}
+
+class Chimpanze extends Animal{
+  constructor(legs, is_warm_blooded){
+      super(legs, is_warm_blooded)
+      this._superPower = new SuperPower();
+  }
+}
+
+class Fox extends Animal{
+  constructor(legs, is_warm_blooded){
+      super(legs, is_warm_blooded)
+      this._superPower = new SuperPower();
+  }
+}
+
+class Chicken extends Animal{
+  constructor(legs, is_warm_blooded){
+      super(legs, is_warm_blooded)
+      this._superPower = new SuperPower();
+  }
+}
+
+class SuperPower{
+  constructor(){
+  }
+
+
+  be_invisible(){
+    return console.log(`Binatang bisa menghilang`);
+  }
+}
+
+
+let batman = new Bat(2, 'warm');
+batman._superPower.be_invisible();
+//console.log(batman.parent().this._numlegs);
