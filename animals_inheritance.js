45644c4b8ecf602e133b41ebf407fcb026e603ed
dyankastutara <dyankastutara19@gmail.com>
2017-04-04 14:06:47 +0700

    dyankastutara done

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,41 @@
 "use strict"
+
+class Animal{
+  constructor(name,num_legs){
+    this.name = name;
+    this.num_legs = num_legs;
+  }
+}
+
+class Fox extends Animal{
+  constructor(name,num_legs,blood){
+    super(name,num_legs)
+    this.warm_blood = 'cold';
+    this.power = new SuperPower();
+  }
+}
+
+class Chicken extends Animal{
+  constructor(name,num_legs,blood){
+    super(name,num_legs)
+    this.warm_blood = blood;
+    this.power = new SuperPower();
+  }
+}
+
+class SuperPower{
+  constructor(){
+
+  }
+  use_laser_vision(){
+    return "Laser";
+  }
+  be_invisible(){
+    return "Invisible";
+  }
+}
+
+let fox = new Fox('Foxes',4)
+let chick = new Chicken('Chick',2)
+console.log(`${fox.name} , ${fox.num_legs}, ${fox.warm_blood}, ${fox.power.use_laser_vision()}`);
+console.log(`${chick.name} , ${chick.num_legs}, ${chick.warm_blood}, ${chick.power.use_laser_vision()}`);
