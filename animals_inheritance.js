8563a9847d2d1b5047258a28e314b379486b524d
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-04-08 19:41:17 +0700

    Komit

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -2,40 +2,54 @@
 class Animal {
   constructor(name, domesticated, num_legs) {
     this.name = name;
-    this.domesticated = domesticated
-    this.num_legs = num_legs
+    this.domesticated = domesticated || "true";
+    this.num_legs = num_legs;
   }
   move (){
-    return console.log("This animal is moving");
+    console.log(`${this.name} is moving`);
   }
 }
 
 class Cat extends Animal {
-  constructor(name, domesticated, num_legs, has_tail, has_fur) {
-    super(name, domesticated, num_legs);
-    this.has_tail = has_tail;
-    this.has_fur = has_fur;
+  constructor(name,domesticated, num_legs) {
+    //super(name,num_legs);
+    super(name);
+    this.superPower = new SuperPower(name);
+  }
+  move(){
+    super.move();
   }
 }
 
-class Eagle {
-  constructor(name, domesticated, num_legs, is_flying) {
-    super(name, domesticated, num_legs);
-    this.is_flying = is_flying;
+class Eagle extends Animal {
+  constructor(name, domesticated) {
+    //super(name, domesticated, num_legs);
+    super(name);
+    this.superPower = new SuperPower(name);
+  }
+  move(){
+    super.move();
   }
 }
 
 class SuperPower {
-  constructor() {
+  constructor(name) {
+    this.name = name;
   }
   use_laser_vision(){
-    return console.log('${this.name} use lser vision')
+    console.log(`${this.name} use laser vision`);
   }
 
   use_fast_healing(){
-    return console.log('${this.name} use fast healing')
+    console.log(`${this.name} use fast healing`);
   }
 }
 
-let Eagl = new Eagle(false, 0, true);
-Eagl.move;
+let Cate = new Cat("Kat", 4);
+console.log(Cate.name);
+Cate.move();
+console.log(Cate.domesticated);
+let Eagl = new Eagle("Elang", false);
+
+Eagl.superPower.use_laser_vision();
+Cate.superPower.use_fast_healing();
